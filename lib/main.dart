7570c8c99f5d8102f575f4269085c356d007ad41
boyallex <60341259+boyallex@users.noreply.github.com>
2021-06-28 16:11:44 +0300

    Dev

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,33 @@
+// import 'dart:html';
+
 import 'package:flow_tok/routes/StartScreen.dart';
 import 'package:flutter/material.dart';
-// import './routes/StartScreen' as StartScreen;
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      
-     home: StartScreen()
-     );
+      home: MyAppView(),
+    );
+  }
+}
+
+class MyAppView extends StatefulWidget {
+  const MyAppView({Key? key}) : super(key: key);
+
+  @override
+  _MyAppViewState createState() => _MyAppViewState();
+}
+
+class _MyAppViewState extends State<MyAppView> {
+  @override
+  Widget build(BuildContext context) {
+    return Entering();
   }
 }
--- lib/routes/StartScreen.dart
+++ lib/routes/StartScreen.dart
@@ -2,32 +2,129 @@
 
 import 'package:flutter/material.dart';
 
-class StartScreen extends StatelessWidget {
-  const StartScreen();
+class Entering extends StatefulWidget {
+  const Entering({Key? key}) : super(key: key);
 
+  @override
+  _EnteringState createState() => _EnteringState();
+}
+
+class _EnteringState extends State<Entering> {
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+  bool isTouched = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        child: ListView(
-          children: [
-            Container(
-              child: Stack(
-                children: <Widget>[
-                  Image.asset("assets/img/people1.jpg"),
-
-                  Center(
-                    child: Text("data"),
-                  )
-                ],
+      key: scaffoldKey,
+      body: Column(
+        children: [
+          AnimatedContainer(
+            // curve: Curves.bounceOut,
+            height: !isTouched
+                ? MediaQuery.of(context).size.height / 2
+                : MediaQuery.of(context).size.height,
+            duration: Duration(milliseconds: 600),
+            child: Container(
+              color: Colors.blue,
+              child: Center(
+                child: isTouched
+                    ? Container()
+                    : Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(22.0),
+                          child: SizedBox(
+                            child: Text(
+                              "Хочу разместить рекламу",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 24,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                            width: 204,
+                          ),
+                        ),
+                        FloatingActionButton.extended(
+                            backgroundColor: Colors.white,
+                            onPressed: () {
+                              setState(() {
+                                isTouched = true;
+                              });
+                              scaffoldKey.currentState!
+                                  .showBottomSheet(
+                                    (context) {
+                                      return Container(
+                                        height: 300,
+                                      );
+                                    },
+                                  )
+                                  .closed
+                                  .whenComplete(
+                                    () => setState(
+                                      () {
+                                        isTouched = false;
+                                      },
+                                    ),
+                                  );
+                            },
+                            label: Text(
+                              "Я рекламодатель",
+                              style: TextStyle(color: Colors.black),
+                            ),
+                          ),
+                      ],
+                      mainAxisAlignment: MainAxisAlignment.center,
+                    ),
               ),
             ),
-            Container(),
-          ],
-        ),
+          ),
+          AnimatedContainer(
+            // curve: Curves.bounceOut,
+            height: isTouched ? 0 : MediaQuery.of(context).size.height / 2,
+            duration: Duration(milliseconds: 600),
+            child: Container(
+              color: Colors.deepOrangeAccent,
+              child: Center(
+                child: SizedBox(
+                  width: 175,
+                  child: FloatingActionButton.extended(
+                    backgroundColor: Colors.white,
+                    onPressed: () {
+                      setState(() {
+                        isTouched = true;
+                      });
+                      scaffoldKey.currentState!
+                          .showBottomSheet(
+                            (context) {
+                              return Container(
+                                height: 300,
+                              );
+                            },
+                          )
+                          .closed
+                          .whenComplete(
+                            () => setState(
+                              () {
+                                isTouched = false;
+                              },
+                            ),
+                          );
+                    },
+                    label: Text(
+                      "Я блогер",
+                      style: TextStyle(
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
+    ;
   }
 }
